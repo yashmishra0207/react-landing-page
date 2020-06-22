fd6801ea067b08d8ca13ee13edfc1d45f621c4f4
yashmishra0207 <yashmishra0207@gmail.com>
2020-06-22 19:49:01 +0530

    Navbar Completed
    also did some minor changes in index.html adn app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React,{Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navigator from "./components/Navbar";
+import Corousel from "./components/carousel";
 
 class App extends Component {
+  state = {
+    totalItems: "5",
+    userName: "Anand",
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight/2,
+  };
+
   render() {
-    return(
-      <h1>Setup Done</h1>
+    return (
+      <React.Fragment>
+        <Navigator
+          TotalItems={this.state.totalItems}
+          UserName={this.state.userName}
+        />
+        <Corousel
+          ScreenWidth={this.state.screenWidth}
+          ScreenHeight={this.state.screenHeight}
+        />
+      </React.Fragment>
     );
   }
 }
